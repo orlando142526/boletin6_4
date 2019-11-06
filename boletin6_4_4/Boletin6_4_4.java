7a08f4c585f3ef988c9275b846cf62de49490ccf
Orlando Joel Yagual Endara <DANIELCASTELAO\oyagualendara@nautilus31.ad.danielcastelao.org>
2019-11-06 11:40:31 +0100

    first commit

---
+++ boletin6_4_4/Boletin6_4_4.java
@@ -0,0 +1,13 @@
+
+package boletin6_4_4;
+
+public class Boletin6_4_4 {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+   Persona datos = new Persona();
+        datos.calcularPeso();
+    }
+    
+}
